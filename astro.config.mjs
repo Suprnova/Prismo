bc0e59ae5c6e0279eb6d458a8e901b67f2ed9d34
Nova <nova@suprnova.dev>
2022-09-23 17:59:58 -0400

    Prepare for domain change

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
-  site: 'https://suprnova.github.io',
-  base: '/Prismo'
+  site: 'https://prismo.at'
 });
